c2a1c7316d66fc7102ebaeb0f721fc938ac29f0b
Bryan <bryanwroberts@bwr.local>
2015-08-31 08:16:51 +0100

    pdf-tools

---
+++ elpa/tablist-20150507.111/tablist-pkg.el
@@ -0,0 +1,7 @@
+(define-package "tablist" "20150507.111" "Extended tabulated-list-mode"
+  '((emacs "24.3"))
+  :keywords
+  '("extensions" "lisp"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
